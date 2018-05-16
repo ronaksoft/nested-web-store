1c527b0b24128f2d8c479b0e9e0deb62f92f2cd2
Hamidreza KK <hamidrezakks@gmail.com>
2018-05-16 11:52:35 +0430

    User list updated

--- src/app/api/user/index.tsx
+++ src/app/api/user/index.tsx
@@ -3,10 +3,9 @@ import Const from '../consts/CServer';
 import {IUser} from 'api/interfaces';
 
 class UserFactory {
-
-  public getAll(type: string): Promise<IUser[]> {
-    return axios.post(Const.SERVER_URL + '/admin/user/' + type, {}).then((response) => {
-      return response.data.data.apps;
+  public get(id: string): Promise<any> {
+    return axios.post(Const.SERVER_URL + '/admin/user/get/' + id, {}).then((response) => {
+      return response.data.data.user;
     });
   }
 
@@ -24,6 +23,13 @@ class UserFactory {
 
   public edit(model: IUser) {
     return axios.post(Const.SERVER_URL + '/admin/user/edit', model).then((response) => {
+      return response.data.data;
+    });
+  }
+
+  public getAll(query: string, status: number = 0, skip: number = 0, limit: number = 20): Promise<any> {
+    return axios.post(Const.SERVER_URL + '/admin/user/search',
+      {query, status, skip, limit}).then((response) => {
       return response.data.data;
     });
   }
